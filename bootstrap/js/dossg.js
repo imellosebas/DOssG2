f79a2deeca0a202696b49e756d3d017ce2f39a78
Sebas Bedoya Cortès <juli.gutti@hotmail.com>
2012-12-01 08:51:36 -0500

    imagenes

--- bootstrap/js/dossg.js
+++ bootstrap/js/dossg.js
@@ -1,6 +1,8 @@
-(function() {
 
-  StackMob.init({
+(function(){
+    
+   var StackMob=$();
+   StackMob.init({
     appName: "dossg",
     clientSubdomain: "sebasbecohotmailcom",
     publicKey: "f06020df-4357-42c5-8ac2-0b8277add26c",
@@ -15,13 +17,13 @@
    var nombre = $('#nombre').val();
    var tipo_documento = $('#tipo_documento') .val();
    var documento =  $('#documento').val();
-   var Direccion =  $('#Direccion').val();
-   var Telefono =  $('#Telefono').val();
+   var direccion =  $('#Direccion').val();
+   var telefono =  $('#Telefono').val();
    var email =  $('#email').val();
    var monto_donado =  $('#monto_donado').val();
   
-   var entry = new donar({ direccion: Direccion, nombre: nombre, tipo_documento: tipo_documento, 
-   documento: documento, telefono: Telefono, email: email, monto_donado: monto_donado  });
+   var entry = new donar({ direccion: direccion, nombre: nombre, tipo_documento: tipo_documento, 
+   documento: documento, telefono: telefono, email: email, monto_donado: monto_donado });
     entry.create();
     
     console.log("exacto");
@@ -31,7 +33,7 @@
 
 
 
-var donar = StackMob.Model.extend({ schemaName: 'donar' });
+var donacion = StackMob.Model.extend({ schemaName: 'donar' });
 
      $('#donacion').click(function(e) {
    e.preventDefault();
@@ -42,7 +44,7 @@ var donar = StackMob.Model.extend({ schemaName: 'donar' });
    var telefono =  $('#telefono').val();
    var Email =  $('#Email').val();
    
-    var entry = new donar({ nombre: nombres, producto: producto, direccion: direccion, telefono: telefono, email: Email });
+    var entry = new donacion({ nombre: nombres, producto: producto, direccion: direccion, telefono: telefono, email: Email });
     entry.create();
     
     console.log("exacto");
@@ -56,6 +58,7 @@ var adoptar = StackMob.Model.extend({ schemaName: 'adoptar' });
 $('#adoptar').click(function(e) {
    e.preventDefault();
    
+   var mascota = $('#mascota'). val();
    var name = $('#name').val();
    var tipodocumento = $('#tipodocumento') .val();
    var numer =  $('#numer').val();
@@ -63,33 +66,10 @@ $('#adoptar').click(function(e) {
    var dicc =  $('#dicc').val();
    var correo =  $('#coreo').val();
    
-    var entry = new adoptar({ nombre: name, tipdocumento: tipodocumento, documento: numer, telefono: tel, email: correo, direccion: dicc });
+    var entry = new adoptar({ nombre: name, tipdocumento: tipodocumento, documento: numer, telefono: tel, email: correo, direccion: dicc, mascota: mascota });
     entry.create();
     
     console.log("exacto");
   
 });
-
-
-
-  $('#create-coderiser').click(function(e) {
-    e.preventDefault();
-
-    var Coderiser = StackMob.Model.extend({ schemaName: 'coderisers' });
-    var entry = new Coderiser({ name: 'Coderiser :D', passions: [ 'code', 'fun', 'javascript', 'html', 'css' ] });
-    entry.create();
-
-    console.log("Created a coderiser in StackMob server");
-
-    coderisers = new Coderiser();
-
-    coderisers.fetch({
-      success: function(model) {
-        console.log(model.toJSON());
-      },
-      error: function(mode, response) {
-        console.log(response);
-      }
-    });
-
-  });
+        });
